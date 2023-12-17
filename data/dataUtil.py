b23c13e5601264c78b0c714841594b3a7cb23a14
Callum Takasaka <ctakasaka@outlook.com>
2023-12-17 11:30:04 -0800

    Removed unnecessary function

--- data/dataUtil.py
+++ data/dataUtil.py
@@ -32,28 +32,6 @@ class Volume:
 
         raise IndexError("location " + str(location) + " does not lie inside volume")
 
-    def __setitem__(self, location, value):
-        """Set the closest value of this volume to the given location. The 
-        location is in world units, relative to the volumes offset.
-
-        This method takes into account the resolution of the volume. An 
-        IndexError exception is raised if the location is not contained in this 
-        volume.
-
-        To access the raw pixel values, use the `data` attribute.
-        """
-
-        i = tuple([ round(location[d]/self.resolution[d]) for d in range(len(location)) ])
-
-        if min(i) >= 0:
-            try:
-                self.data[i] = value
-                return
-            except IndexError as e:
-                raise IndexError("location " + str(location) + " does not lie inside volume: " + str(e))
-
-        raise IndexError("location " + str(location) + " does not lie inside volume")
-
 
 def generate_sparse_masks(gt_segmentation, subsampling_ratio=0.1):
 
