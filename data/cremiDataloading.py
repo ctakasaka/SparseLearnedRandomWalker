b8dee8c43990815515dc9dea51f379c050dc7613
Callum Takasaka <ctakasaka@outlook.com>
2023-12-17 12:04:38 -0800

    cropping to prevent test set leakage

--- data/cremiDataloading.py
+++ data/cremiDataloading.py
@@ -8,7 +8,9 @@ from torch.utils.data import Dataset, DataLoader
 # raw & segmentation combined dataset object
 class CremiSegmentationDataset(Dataset):
 
-  def __init__(self, cremi_location, transform=None, target_transform=None, subsampling_ratio=0.1):
+  def __init__(self, cremi_location, transform=None, target_transform=None, subsampling_ratio=0.1, testing=False):
+
+    self.testing = testing
 
     cremi_hdf = h5py.File(cremi_location, "r")
     raw_dataset = Volume(cremi_hdf["/volumes/raw"])
@@ -33,15 +35,23 @@ class CremiSegmentationDataset(Dataset):
   
   def __getitem__(self, idx):
 
+    crop_idx = 4
+    if not self.testing:
+      rng = np.random.default_rng()
+      crop_idx = rng.integers(4, size=1)[0]
+
     raw_neurons = self.raw[idx]
     seg_neurons = self.seg[idx]
     sample_mask = self.mask[idx]
 
     if self.transform:
       raw_neurons = self.transform(raw_neurons)
+      raw_neurons = raw_neurons[crop_idx]
     if self.target_transform:
       seg_neurons = self.target_transform(seg_neurons)
       sample_mask = self.target_transform(sample_mask)
+      seg_neurons = seg_neurons[crop_idx]
+      sample_mask = sample_mask[crop_idx]
 
     return raw_neurons, seg_neurons, sample_mask
   
--- exampleDataloader.py
+++ exampleDataloader.py
@@ -8,30 +8,24 @@ import matplotlib.pyplot as plt
 
 # transforms
 raw_transforms = transforms.Compose([
-    # transforms.ToTensor(),
-    transforms.CenterCrop(size=(256, 256)),
     transforms.Normalize(mean=[0.5], std=[0.5]),
+    transforms.FiveCrop(size=(256, 256)),
 ])
 target_transforms = transforms.Compose([
-    # transforms.ToTensor(),
-    transforms.CenterCrop(size=(256, 256)),
+    transforms.FiveCrop(size=(256, 256)),
 ])
 
 # loading in dataset
-train_dataset = CremiSegmentationDataset("data/sample_A_20160501.hdf", transform=raw_transforms, target_transform=target_transforms, subsampling_ratio=0.1)
+train_dataset = CremiSegmentationDataset("data/sample_A_20160501.hdf", transform=raw_transforms, target_transform=target_transforms, subsampling_ratio=0.1, testing=False)
 
-# setting up dataloader (should give a length of 5, 125 / 25)
-# shuffle should be okay, since the segmentation & raw are tupled now
+# setting up dataloader
 train_dataloader = DataLoader(train_dataset, batch_size=25, shuffle=True)
-
-print(f"Dataloader should have a length of 125 / 25: {len(train_dataloader)}")
 
 # sanity check
 figure = plt.figure(figsize=(8, 8))
 cols, rows = 2, 2
 for i in range(1, cols * rows + 1):
-    # sample_idx = torch.randint(len(train_dataset), size=(1,)).item()
-    sample_idx = i
+    sample_idx = torch.randint(len(train_dataset), size=(1,)).item()
     img, segmentation, mask = train_dataset[sample_idx]
     figure.add_subplot(rows, cols, i)
     plt.axis("off")
