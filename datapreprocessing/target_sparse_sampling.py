e5f8d2499b2f76eff0c01e4d231f71f657a6e9f7
sonalbihani <sonalbihani9@gmail.com>
2023-12-19 14:33:37 -0500

    change sparse mask to numpy and add sampling of 1 seed for unique classes first

--- datapreprocessing/target_sparse_sampling.py
+++ datapreprocessing/target_sparse_sampling.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import numpy as np
 
 class SparseMaskTransform(object):
     def __init__(self, subsampling_ratio=0.1):
@@ -13,15 +14,22 @@ class SparseMaskTransform(object):
             target_mask = torch.tensor(target_mask, dtype=torch.int)
 
         # Flatten the mask
-        flat_mask = target_mask.ravel()
-
-        # Create a sparse mask by subsampling
+        flat_mask = target_mask.ravel().numpy()
+         # Create a sparse mask by subsampling
+        sparse_target = np.zeros_like(flat_mask)
+        unique_classes = np.unique(flat_mask)
         num_elements = flat_mask.shape[0]
-        num_samples = int(self.subsampling_ratio * num_elements)
-        sampled_indices = random.sample(range(num_elements), num_samples)
-        sparse_target = torch.zeros_like(flat_mask)
+        for class_label in unique_classes:
+            class_indices = np.where(flat_mask == class_label)[0]
+            if len(class_indices) > 0:
+                seed_index = random.choice(class_indices)
+                sparse_target[seed_index] = 1
+        num_samples = int(self.subsampling_ratio * num_elements) - len(unique_classes)
+        
+        sampled_indices = np.random.choice(np.arange(sparse_target.shape[0]), 
+                               replace=False, 
+                               size=num_samples)
         sparse_target[sampled_indices] = 1
 
-        return sparse_target.view(target_mask.size())
-
-
+        # fix mask shapes & cast to tensor
+        return torch.from_numpy((sparse_target.reshape(target_mask.shape)).astype(np.int64)).to(torch.int8)
