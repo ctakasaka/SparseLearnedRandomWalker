82ab7d8dd21caac130d9da0d106652101fb9be6a
Callum Takasaka <cttakasaka@uwaterloo.ca>
2023-12-20 11:41:23 -0800

    Fixing up notebook test script

--- testPretrainedModel.py
+++ testPretrainedModel.py
@@ -1,20 +1,17 @@
 import numpy as np
 import torch
-from torchvision import transforms
 from unet.unet import UNet
 from randomwalker.RandomWalkerModule import RandomWalker
 from data.cremiDataloading import CremiSegmentationDataset
 from datapreprocessing.target_sparse_sampling import SparseMaskTransform
-
-from randomwalker.randomwalker_loss_utils import NHomogeneousBatchLoss
+from utils.notebookUtils import make_summary_plot, sample_seeds, generate_transforms
 from utils.evaluation import compute_iou
-from typing import Dict
-import matplotlib.pyplot as plt
 
-from utils.notebookUtils import make_summary_plot, sample_seeds
 
-subsampling_ratio = 0.01
-seeds_per_region = 5
+image_size = 128         # note that the image shall be square (and preferably even)
+seeds_per_region = 5     # number of seeds provided 
+
+raw_transforms, target_transforms = generate_transforms(image_size=image_size)
 
 model_path = "checkpoints/models/last_model_20231219_165700_9"
 
@@ -23,28 +20,24 @@ model.load_state_dict(torch.load(model_path))
 model.eval()
 rw = RandomWalker(1000, max_backprop=True)
 
-raw_transforms = transforms.Compose([
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-    transforms.FiveCrop(size=(512, 512)),
-])
-target_transforms = transforms.Compose([
-    transforms.FiveCrop(size=(512, 512)),
-])
-
 # load a test observation
 test_dataset = CremiSegmentationDataset("data/sample_A_20160501.hdf", transform=raw_transforms, target_transform=target_transforms, testing=True)
 test_raw, test_segmentation, test_mask = test_dataset[93]
+
+# determine number of segments in the image
 num_classes = len(np.unique(test_segmentation.squeeze()))
 
-diffusivities = model(test_raw.unsqueeze(0))
-# Diffusivities must be positive
-net_output = torch.sigmoid(diffusivities)
+# sample some amount of seeds from each segment
+seeds = sample_seeds(seeds_per_region, test_segmentation, num_classes)
 
-mask = SparseMaskTransform(subsampling_ratio)(test_segmentation.squeeze())
-mask_x, mask_y = mask.nonzero(as_tuple=True)
-masked_targets = test_segmentation.squeeze()[mask_x, mask_y]
+# compute UNet output
+net_output = model(test_raw.unsqueeze(0))
+diffusivities = torch.sigmoid(net_output)
+# RW given the diffusivities from UNet
+output = rw(diffusivities, seeds)
 
-seeds = sample_seeds(seeds_per_region, test_segmentation, masked_targets, mask_x, mask_y, num_classes)
-output = rw(net_output, seeds)
+# compute fit metrics (mIoU)
+pred_masks = torch.argmax(output[0], dim=1)
+iou_score = compute_iou(pred_masks.detach().cpu(), test_segmentation.detach().cpu(), num_classes)
 
-make_summary_plot(0, test_raw, output, net_output, seeds, test_segmentation, mask, subsampling_ratio, 0)
+make_summary_plot(0, test_raw, output, diffusivities, seeds, test_segmentation, iou_score, model_subsampling_ratio=0.01)
--- utils/notebookUtils.py
+++ utils/notebookUtils.py
@@ -1,13 +1,14 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from torchvision import transforms
 
-def make_summary_plot(it, raw, output, net_output, seeds, target, mask, subsampling_ratio, epoch_index):
+def make_summary_plot(it, raw, output, net_output, seeds, target, miou, model_subsampling_ratio):
         """
         This function create and save a summary figure
         """
         f, axarr = plt.subplots(2, 2, figsize=(8, 9.5))
-        f.suptitle("RW summary, Iteration: " + repr(it))
+        f.suptitle(f"Model Pretrained on {model_subsampling_ratio} Subsampling")
 
         axarr[0, 0].set_title("Ground Truth Image")
         axarr[0, 0].imshow(raw[0].detach().numpy(), cmap="gray")
@@ -17,21 +18,16 @@ def make_summary_plot(it, raw, output, net_output, seeds, target, mask, subsampl
                             seeds_listx, c="r")
         axarr[0, 0].axis("off")
 
-        mask_x, mask_y = np.where(mask != 0)
-        axarr[0, 0].scatter(mask_y,
-                            mask_x, c="b", s=0.5, marker='o')
-        axarr[0, 0].axis("off")
-
-        axarr[0, 1].set_title("LRW output (white seed)")
+        axarr[0, 1].set_title(f"SLRW Prediction\nMean IoU: {round(miou, 4)}")
         axarr[0, 1].imshow(raw[0].detach().numpy(), cmap="gray")
         axarr[0, 1].imshow(np.argmax(output[0][0].detach().numpy(), 0), alpha=0.6, vmin=-3, cmap="prism_r")
         axarr[0, 1].axis("off")
 
-        axarr[1, 0].set_title("Vertical Diffusivities")
+        axarr[1, 0].set_title("Vertical Diffusivities (UNet)")
         axarr[1, 0].imshow(net_output[0, 0].detach().numpy(), cmap="gray")
         axarr[1, 0].axis("off")
 
-        axarr[1, 1].set_title("Horizontal Diffusivities")
+        axarr[1, 1].set_title("Horizontal Diffusivities (UNet)")
         axarr[1, 1].imshow(net_output[0, 1].detach().numpy(), cmap="gray")
         axarr[1, 1].axis("off")
 
@@ -42,11 +38,36 @@ def make_summary_plot(it, raw, output, net_output, seeds, target, mask, subsampl
         # plt.savefig(f"./results-full/{subsampling_ratio}/epoch-{epoch_index}/{it}.png")
         # plt.close()
 
-def sample_seeds(seeds_per_region, target, masked_target, mask_x, mask_y, num_classes):
+def sample_seeds(seeds_per_region, target, num_classes):
+        
+        target_temp = target.squeeze()
+        flat_target = target_temp.reshape((-1))
+        full_mask = torch.ones_like(target_temp)
+        mask_x, mask_y = full_mask.nonzero(as_tuple=True)
+
+        seeds = torch.zeros_like(target_temp)
+        if seeds_per_region == 1:
+            seed_indices = np.array([
+                np.random.choice(np.where(flat_target == i)[0]) for i in range(num_classes)
+            ])
+        else:
+            num_seeds = [
+                min(len(np.where(flat_target == i)[0]), seeds_per_region) for i in range(num_classes)
+            ]
+            seed_indices = np.concatenate([
+                np.random.choice(np.where(flat_target == i)[0], num_seeds[i], replace=False)
+                for i in range(num_classes)
+            ])
+        target_seeds = target.squeeze()[mask_x[seed_indices], mask_y[seed_indices]] + 1
+        seeds[mask_x[seed_indices], mask_y[seed_indices]] = target_seeds
+        seeds = seeds.unsqueeze(0)
+        return seeds
+
+def bsdfsample_seeds(seeds_per_region, target, masked_target, mask_x, mask_y, num_classes):
         seeds = torch.zeros_like(target.squeeze())
         if seeds_per_region == 1:
             seed_indices = np.array([
-                np.random.choice(np.where(masked_target == i)[0]) for i in range(num_classes)
+                np.random.choice(np.where(target == i)[0]) for i in range(num_classes)
             ])
         else:
             num_seeds = [
@@ -60,3 +81,24 @@ def sample_seeds(seeds_per_region, target, masked_target, mask_x, mask_y, num_cl
         seeds[mask_x[seed_indices], mask_y[seed_indices]] = target_seeds
         seeds = seeds.unsqueeze(0)
         return seeds
+
+def generate_transforms(image_size=128):
+        
+        if image_size < 128:
+            print(f"Image size of {image_size} too small. Setting to 128x128.")
+            image_size = 128
+        elif image_size > 512:
+            print(f"Image size of {image_size} too large. Setting to 512x512.")
+            image_size = 512
+        elif image_size % 2 != 0:
+            image_size -= 1
+
+        raw_transforms = transforms.Compose([
+            transforms.Normalize(mean=[0.5], std=[0.5]),
+            transforms.FiveCrop(size=(image_size, image_size)),
+        ])
+        target_transforms = transforms.Compose([
+            transforms.FiveCrop(size=(image_size, image_size)),
+        ])
+
+        return raw_transforms, target_transforms
